85d846f91534324d4345f584d347d2c341461eab
Akhilbabu456 <akhilbeliever001@gmail.com>
2026-03-14 19:37:18 +0530

    Updates

--- src/utils/encryptedStorage.js
+++ src/utils/encryptedStorage.js
@@ -1,6 +1,7 @@
-import {EncryptStorage} from "encrypt-storage";
+import { EncryptStorage } from "encrypt-storage";
 
-const secretKey = import.meta.env.VITE_APP_LOCALSTORAGE_KEY;
+const secretKey =
+  import.meta.env.VITE_APP_LOCALSTORAGE_KEY || "fallback_development_key_123";
 
 export const encryptedStorage = new EncryptStorage(secretKey, {
   storageType: "localStorage",
